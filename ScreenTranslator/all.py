ccd635d981bdbd1fe83871e988aa30c4c40cbc4b
azoozs <82043121+azoozs@users.noreply.github.com>
2024-05-14 22:06:08 +0300

    Add files via upload

---
+++ ScreenTranslator/all.py
@@ -0,0 +1,431 @@
+all_langs = {
+    "Afrikaans": {
+        "googletrans": "af",
+        "pytesseract": "afr"
+    },
+    "Albanian": {
+        "googletrans": "sq",
+        "pytesseract": "sqi"
+    },
+    "Amharic": {
+        "googletrans": "am",
+        "pytesseract": "amh"
+    },
+    "Arabic": {
+        "googletrans": "ar",
+        "pytesseract": "ara"
+    },
+    "Armenian": {
+        "googletrans": "hy",
+        "pytesseract": "hye"
+    },
+    "Azerbaijani": {
+        "googletrans": "az",
+        "pytesseract": "aze"
+    },
+    "Basque": {
+        "googletrans": "eu",
+        "pytesseract": "eus"
+    },
+    "Belarusian": {
+        "googletrans": "be",
+        "pytesseract": "bel"
+    },
+    "Bengali": {
+        "googletrans": "bn",
+        "pytesseract": "ben"
+    },
+    "Bosnian": {
+        "googletrans": "bs",
+        "pytesseract": "bos"
+    },
+    "Bulgarian": {
+        "googletrans": "bg",
+        "pytesseract": "bul"
+    },
+    "Catalan": {
+        "googletrans": "ca",
+        "pytesseract": "cat"
+    },
+    "Cebuano": {
+        "googletrans": "ceb",
+        "pytesseract": "ceb"
+    },
+    "Chichewa": {
+        "googletrans": "ny",
+        "pytesseract": "not supported"
+    },
+    "Chinese (simplified)": {
+        "googletrans": "zh-cn",
+        "pytesseract": "chi_sim"
+    },
+    "Chinese (traditional)": {
+        "googletrans": "zh-tw",
+        "pytesseract": "chi_tra"
+    },
+    "Corsican": {
+        "googletrans": "co",
+        "pytesseract": "cos"
+    },
+    "Croatian": {
+        "googletrans": "hr",
+        "pytesseract": "hrv"
+    },
+    "Czech": {
+        "googletrans": "cs",
+        "pytesseract": "ces"
+    },
+    "Danish": {
+        "googletrans": "da",
+        "pytesseract": "dan"
+    },
+    "Dutch": {
+        "googletrans": "nl",
+        "pytesseract": "nld"
+    },
+    "English": {
+        "googletrans": "en",
+        "pytesseract": "eng"
+    },
+    "Esperanto": {
+        "googletrans": "eo",
+        "pytesseract": "epo"
+    },
+    "Estonian": {
+        "googletrans": "et",
+        "pytesseract": "est"
+    },
+    "Filipino": {
+        "googletrans": "tl",
+        "pytesseract": "fil"
+    },
+    "Finnish": {
+        "googletrans": "fi",
+        "pytesseract": "fin"
+    },
+    "French": {
+        "googletrans": "fr",
+        "pytesseract": "fra"
+    },
+    "Frisian": {
+        "googletrans": "fy",
+        "pytesseract": "fry"
+    },
+    "Galician": {
+        "googletrans": "gl",
+        "pytesseract": "glg"
+    },
+    "Georgian": {
+        "googletrans": "ka",
+        "pytesseract": "kat"
+    },
+    "German": {
+        "googletrans": "de",
+        "pytesseract": "deu"
+    },
+    "Greek": {
+        "googletrans": "el",
+        "pytesseract": "ell"
+    },
+    "Gujarati": {
+        "googletrans": "gu",
+        "pytesseract": "guj"
+    },
+    "Haitian creole": {
+        "googletrans": "ht",
+        "pytesseract": "hat"
+    },
+    "Hausa": {
+        "googletrans": "ha",
+        "pytesseract": "not supported"
+    },
+    "Hawaiian": {
+        "googletrans": "haw",
+        "pytesseract": "not supported"
+    },
+    "Hebrew": {
+        "googletrans": "he",
+        "pytesseract": "heb"
+    },
+    "Hindi": {
+        "googletrans": "hi",
+        "pytesseract": "hin"
+    },
+    "Hmong": {
+        "googletrans": "hmn",
+        "pytesseract": "not supported"
+    },
+    "Hungarian": {
+        "googletrans": "hu",
+        "pytesseract": "hun"
+    },
+    "Icelandic": {
+        "googletrans": "is",
+        "pytesseract": "isl"
+    },
+    "Igbo": {
+        "googletrans": "ig",
+        "pytesseract": "not supported"
+    },
+    "Indonesian": {
+        "googletrans": "id",
+        "pytesseract": "ind"
+    },
+    "Irish": {
+        "googletrans": "ga",
+        "pytesseract": "gle"
+    },
+    "Italian": {
+        "googletrans": "it",
+        "pytesseract": "ita"
+    },
+    "Japanese": {
+        "googletrans": "ja",
+        "pytesseract": "jpn"
+    },
+    "Javanese": {
+        "googletrans": "jw",
+        "pytesseract": "jav"
+    },
+    "Kannada": {
+        "googletrans": "kn",
+        "pytesseract": "kan"
+    },
+    "Kazakh": {
+        "googletrans": "kk",
+        "pytesseract": "kaz"
+    },
+    "Khmer": {
+        "googletrans": "km",
+        "pytesseract": "khm"
+    },
+    "Korean": {
+        "googletrans": "ko",
+        "pytesseract": "kor"
+    },
+    "Kurdish (kurmanji)": {
+        "googletrans": "ku",
+        "pytesseract": "not supported"
+    },
+    "Kyrgyz": {
+        "googletrans": "ky",
+        "pytesseract": "kir"
+    },
+    "Lao": {
+        "googletrans": "lo",
+        "pytesseract": "lao"
+    },
+    "Latin": {
+        "googletrans": "la",
+        "pytesseract": "lat"
+    },
+    "Latvian": {
+        "googletrans": "lv",
+        "pytesseract": "lav"
+    },
+    "Lithuanian": {
+        "googletrans": "lt",
+        "pytesseract": "lit"
+    },
+    "Luxembourgish": {
+        "googletrans": "lb",
+        "pytesseract": "ltz"
+    },
+    "Macedonian": {
+        "googletrans": "mk",
+        "pytesseract": "mkd"
+    },
+    "Malagasy": {
+        "googletrans": "mg",
+        "pytesseract": "not supported"
+    },
+    "Malay": {
+        "googletrans": "ms",
+        "pytesseract": "msa"
+    },
+    "Malayalam": {
+        "googletrans": "ml",
+        "pytesseract": "mal"
+    },
+    "Maltese": {
+        "googletrans": "mt",
+        "pytesseract": "mlt"
+    },
+    "Maori": {
+        "googletrans": "mi",
+        "pytesseract": "mri"
+    },
+    "Marathi": {
+        "googletrans": "mr",
+        "pytesseract": "mar"
+    },
+    "Mongolian": {
+        "googletrans": "mn",
+        "pytesseract": "mon"
+    },
+    "Myanmar (burmese)": {
+        "googletrans": "my",
+        "pytesseract": "not supported"
+    },
+    "Nepali": {
+        "googletrans": "ne",
+        "pytesseract": "nep"
+    },
+    "Norwegian": {
+        "googletrans": "no",
+        "pytesseract": "nor"
+    },
+    "Odia": {
+        "googletrans": "or",
+        "pytesseract": "not supported"
+    },
+    "Pashto": {
+        "googletrans": "ps",
+        "pytesseract": "pus"
+    },
+    "Persian": {
+        "googletrans": "fa",
+        "pytesseract": "fas"
+    },
+    "Polish": {
+        "googletrans": "pl",
+        "pytesseract": "pol"
+    },
+    "Portuguese": {
+        "googletrans": "pt",
+        "pytesseract": "por"
+    },
+    "Punjabi": {
+        "googletrans": "pa",
+        "pytesseract": "pan"
+    },
+    "Romanian": {
+        "googletrans": "ro",
+        "pytesseract": "ron"
+    },
+    "Russian": {
+        "googletrans": "ru",
+        "pytesseract": "rus"
+    },
+    "Samoan": {
+        "googletrans": "sm",
+        "pytesseract": "not supported"
+    },
+    "Scots gaelic": {
+        "googletrans": "gd",
+        "pytesseract": "not supported"
+    },
+    "Serbian": {
+        "googletrans": "sr",
+        "pytesseract": "srp"
+    },
+    "Sesotho": {
+        "googletrans": "st",
+        "pytesseract": "not supported"
+    },
+    "Shona": {
+        "googletrans": "sn",
+        "pytesseract": "not supported"
+    },
+    "Sindhi": {
+        "googletrans": "sd",
+        "pytesseract": "snd"
+    },
+    "Sinhala": {
+        "googletrans": "si",
+        "pytesseract": "sin"
+    },
+    "Slovak": {
+        "googletrans": "sk",
+        "pytesseract": "slk"
+    },
+    "Slovenian": {
+        "googletrans": "sl",
+        "pytesseract": "slv"
+    },
+    "Somali": {
+        "googletrans": "so",
+        "pytesseract": "not supported"
+    },
+    "Spanish": {
+        "googletrans": "es",
+        "pytesseract": "spa"
+    },
+    "Sundanese": {
+        "googletrans": "su",
+        "pytesseract": "sun"
+    },
+    "Swahili": {
+        "googletrans": "sw",
+        "pytesseract": "swa"
+    },
+    "Swedish": {
+        "googletrans": "sv",
+        "pytesseract": "swe"
+    },
+    "Tajik": {
+        "googletrans": "tg",
+        "pytesseract": "tgk"
+    },
+    "Tamil": {
+        "googletrans": "ta",
+        "pytesseract": "tam"
+    },
+    "Telugu": {
+        "googletrans": "te",
+        "pytesseract": "tel"
+    },
+    "Thai": {
+        "googletrans": "th",
+        "pytesseract": "tha"
+    },
+    "Turkish": {
+        "googletrans": "tr",
+        "pytesseract": "tur"
+    },
+    "Ukrainian": {
+        "googletrans": "uk",
+        "pytesseract": "ukr"
+    },
+    "Urdu": {
+        "googletrans": "ur",
+        "pytesseract": "urd"
+    },
+    "Uyghur": {
+        "googletrans": "ug",
+        "pytesseract": "uig"
+    },
+    "Uzbek": {
+        "googletrans": "uz",
+        "pytesseract": "uzb"
+    },
+    "Vietnamese": {
+        "googletrans": "vi",
+        "pytesseract": "vie"
+    },
+    "Welsh": {
+        "googletrans": "cy",
+        "pytesseract": "cym"
+    },
+    "Xhosa": {
+        "googletrans": "xh",
+        "pytesseract": "not supported"
+    },
+    "Yiddish": {
+        "googletrans": "yi",
+        "pytesseract": "yid"
+    },
+    "Yoruba": {
+        "googletrans": "yo",
+        "pytesseract": "yor"
+    },
+    "Zulu": {
+        "googletrans": "zu",
+        "pytesseract": "not supported"
+    }
+}
+
+
+src_lang_code = list(all_langs.keys())
+print(src_lang_code)
+print(len(all_langs))
